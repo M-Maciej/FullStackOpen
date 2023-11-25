1d38fe798ade2276733952902be38f164c21b8a5
M-Maciej <130112810+M-Maciej@users.noreply.github.com>
2023-11-25 19:41:25 +0100

    exercise 3.1 update

---
+++ part-3/3.1/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const hostname = '127.0.0.1'
+const port = 3001
+
+let persons = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.listen(port, hostname, () => {
+  console.log(`Server running on link http://${hostname}:${port}`)
+})
